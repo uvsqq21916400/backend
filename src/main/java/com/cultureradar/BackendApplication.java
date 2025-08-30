9f7d771475ae6a5c03caab873a724339902fe634
uvsqq21916400 <flaviekutukialanda@gmail.com>
2025-08-30 22:45:59 +0200

    fix: scan JPA repos, entities & properties (UserFavoriteRepo not found)

--- src/main/java/com/cultureradar/BackendApplication.java
+++ src/main/java/com/cultureradar/BackendApplication.java
@@ -2,12 +2,17 @@ package com.cultureradar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.cultureradar")   // scan des interfaces JpaRepository (ex: UserFavoriteRepo)
+@EntityScan(basePackages = "com.cultureradar")               // scan des entités JPA (ex: UserFavorite)
+@ConfigurationPropertiesScan(basePackages = "com.cultureradar") // scan des *Properties (OpenAgendaProperties, UnsplashProperties…)
 public class BackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
 }
+
